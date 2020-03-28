b51cf473e3c64af4215c3a4402fc64f3c8d79e6c
Túlio Castro <tulio.figueiredo@gmail.com>
2020-03-28 11:28:54 -0300

    [client] Added frontend with vuejs

---
+++ client/src/main.js
@@ -0,0 +1,17 @@
+import { BootstrapVue } from 'bootstrap-vue';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Vue from 'vue';
+import App from './App.vue';
+import './assets/global.scss';
+import router from './router';
+import store from './store';
+
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount('#app');
